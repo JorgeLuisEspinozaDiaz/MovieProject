9ebe8b963fb296b35b3250be3724c89b8b6539b0
JorgeDev <espinozadiazjorgeluis@gmail.com>
2023-03-02 21:31:07 -0500

    pagination

--- Movies-Streaming/src/movies/movies.controllers.js
+++ Movies-Streaming/src/movies/movies.controllers.js
@@ -1,66 +1,62 @@
-const Movies = require('../models/movies.models')
-const MovieGenres = require('../models/movie_genres.models')
-const uuid = require('uuid')
-const Genres = require('../models/genres.models')
-
+const Movies = require("../models/movies.models");
+const MovieGenres = require("../models/movie_genres.models");
+const uuid = require("uuid");
+const Genres = require("../models/genres.models");
 
 const findAllMovies = async (limit, offset) => {
-    // limit -> Cuantos quiero mostrar
-    // offset -> Donde empiezo a mostrar
+  // limit -> Cuantos quiero mostrar
+  // offset -> Donde empiezo a mostrar
 
-
-    // const queryOptions = {
-    //     limit: limit || 20,
-    //     offset: offset || 0
-    // }
-    [Op]
-    const data = await Movies.findAndCountAll()
-    return data
-}
+  const queryOptions = {
+    limit: limit,
+    offset: offset,
+  };
+  const data = await Movies.findAndCountAll(queryOptions);
+  return data;
+};
 
 const createMovie = async (movieObj) => {
-    const newMovie = {
-        id: uuid.v4(),
-        title: movieObj.title,
-        synopsis : movieObj.synopsis,
-        releaseYear: movieObj.releaseYear,
-        director: movieObj.director,
-        duration: movieObj.duration,
-        trillerUrl: movieObj.trillerUrl,
-        coverUrl: movieObj.coverUrl,
-        movieUrl: movieObj.movieUrl,
-        classification: movieObj.classification,
-        rating: movieObj.rating
-    }
-    const data = await Movies.create(newMovie)
-    return data
-}
+  const newMovie = {
+    id: uuid.v4(),
+    title: movieObj.title,
+    synopsis: movieObj.synopsis,
+    releaseYear: movieObj.releaseYear,
+    director: movieObj.director,
+    duration: movieObj.duration,
+    trillerUrl: movieObj.trillerUrl,
+    coverUrl: movieObj.coverUrl,
+    movieUrl: movieObj.movieUrl,
+    classification: movieObj.classification,
+    rating: movieObj.rating,
+  };
+  const data = await Movies.create(newMovie);
+  return data;
+};
 
 const addGenreToMovie = async (dataObj) => {
-    const data = await MovieGenres.create({
-        id: uuid.v4(),
-        movieId: dataObj.movieId,
-        genreId: dataObj.genreId
-    })
-    return data
-}
+  const data = await MovieGenres.create({
+    id: uuid.v4(),
+    movieId: dataObj.movieId,
+    genreId: dataObj.genreId,
+  });
+  return data;
+};
 
 const findAllMoviesByGenre = async (genreId) => {
-    const data = await Movies.findAll({
-        include: {
-            model: Genres,
-            where: {
-                id: genreId
-            }
-        }
-    }) 
-    return data
-}
-
+  const data = await Movies.findAll({
+    include: {
+      model: Genres,
+      where: {
+        id: genreId,
+      },
+    },
+  });
+  return data;
+};
 
 module.exports = {
-    findAllMovies,
-    createMovie,
-    addGenreToMovie,
-    findAllMoviesByGenre
-}
+  findAllMovies,
+  createMovie,
+  addGenreToMovie,
+  findAllMoviesByGenre,
+};
